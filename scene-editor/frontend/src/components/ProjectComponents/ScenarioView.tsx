11fdf3267fd40f4da1bd30d644f9ace044549d19
Bjorn <b.jaeken@avans.nl>
2026-04-24 13:05:24 +0200

    Fix issues when new scenario added.

--- scene-editor/frontend/src/components/ProjectComponents/ScenarioView.tsx
+++ scene-editor/frontend/src/components/ProjectComponents/ScenarioView.tsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
-import {range} from 'lodash';
+import { range } from 'lodash';
 
 import { makeStyles, createStyles } from '@mui/styles';
 import { createTheme } from '@mui/material/styles';
@@ -49,11 +49,11 @@ type ScenarioTileProps = {
   setWarningState: any;
 };
 
-const Alert = (props: AlertProps) => {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>((props, ref) => (
+  <MuiAlert elevation={6} variant="filled" ref={ref} {...props} />
+));
 
-const ScenarioSnackbar = ({open, message, severity, handleClose}:any) => {
+const ScenarioSnackbar = ({ open, message, severity, handleClose }: any) => {
   return (
     <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
       <Alert onClose={handleClose} severity={severity}>
@@ -63,7 +63,7 @@ const ScenarioSnackbar = ({open, message, severity, handleClose}:any) => {
   )
 }
 
-const DeleteWarningDialog = ({open, id, handleClose, handleDelete}:any) => {
+const DeleteWarningDialog = ({ open, id, handleClose, handleDelete }: any) => {
   return (
     <Dialog
       open={open}
@@ -94,89 +94,84 @@ const ScenarioTile: React.FC<ScenarioTileProps> = ({ name, activeProject, id, de
   const navigate = useNavigate();
   return (
     <Grid item xs={12} md={6} lg={fullWidth ? 3 : 6} xl={fullWidth ? 3 : 4}>
-      <Card variant="outlined" style={{backgroundColor: '#eeeeee'}}>
+      <Card variant="outlined" style={{ backgroundColor: '#eeeeee' }}>
         <CardHeader
-          action={
-            <IconButton aria-label="settings" size="large">
-                <MoreVertIcon />
-            </IconButton>
-          }
-          titleTypographyProps={{variant:"subtitle1"}}
+          titleTypographyProps={{ variant: "subtitle1" }}
           title={name}
           subheader={created_at}
         />
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
-          {description}
+            {description}
           </Typography>
         </CardContent>
-          <CardActions disableSpacing>
-            <Tooltip title="Edit" arrow>
-                <IconButton
-                  aria-label="edit"
-                  onClick={() => navigate(`/app/scenario-editor/${activeProject}/${id}`)}
-                  size="large">
-                    <EditIcon />
-                </IconButton>
-            </Tooltip>
+        <CardActions disableSpacing>
+          <Tooltip title="Edit" arrow>
+            <IconButton
+              aria-label="edit"
+              onClick={() => navigate(`/app/scenario-editor/${activeProject}/${id}`)}
+              size="large">
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
 
-            <Tooltip title="Remove Scenario">
-              <IconButton
-                aria-label="remove scenario"
-                onClick={() => setWarningState({open: true, id})}
-                size="large">
-                <DeleteIcon />
-              </IconButton>
-            </Tooltip>
-          </CardActions>
-        </Card>
+          <Tooltip title="Remove Scenario">
+            <IconButton
+              aria-label="remove scenario"
+              onClick={() => setWarningState({ open: true, id })}
+              size="large">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </CardActions>
+      </Card>
     </Grid>
   );
 };
 
 
 type ScenarioViewProps = {
-  activeProject:string;
+  activeProject: string;
   fullWidth: boolean;
 }
 
-export default ({activeProject, fullWidth}:ScenarioViewProps) => {
+export default ({ activeProject, fullWidth }: ScenarioViewProps) => {
   const [openScenarioDialog, setOpenScenarioDialog] = useState(false);
   const [loadingScenarios, setLoadingScenarios] = useState(true);
 
   const [scenarios, setScenarios] = useState([]);
 
-  const [warningState, setWarningState] = useState({open: false, id: ""});
-  const [alertState, setAlertState] = useState({open: false, message: "", severity: ""})
+  const [warningState, setWarningState] = useState({ open: false, id: "" });
+  const [alertState, setAlertState] = useState({ open: false, message: "", severity: "" })
 
   const classes = (makeStyles((theme) =>
     createStyles({
-        root: {
-            flexGrow: 1,
-            padding: theme.spacing(2),
-        },
-        paper: {
-          padding: theme.spacing(2),
-          boxSizing: 'border-box'
-        },
-        overview: {
-          height: fullWidth ? 400 : 300,
-          overflow: 'auto'
-        },
-        header: {
-          display: 'flex',
-          alignItems: 'center',
-          flexWrap: 'wrap',
-          fontSize: '1.1rem',
-          fontWeight: 700,
-          color: '#2196f3',
-          marginBottom: 10
-        },
-        cardHeader: {
-          fontSize: '1.2rem'
-        }
+      root: {
+        flexGrow: 1,
+        padding: theme.spacing(2),
+      },
+      paper: {
+        padding: theme.spacing(2),
+        boxSizing: 'border-box'
+      },
+      overview: {
+        height: fullWidth ? 400 : 300,
+        overflow: 'auto'
+      },
+      header: {
+        display: 'flex',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        fontSize: '1.1rem',
+        fontWeight: 700,
+        color: '#2196f3',
+        marginBottom: 10
+      },
+      cardHeader: {
+        fontSize: '1.2rem'
+      }
     }),
-))();
+  ))();
 
   useEffect(() => {
     setLoadingScenarios(true);
@@ -184,47 +179,47 @@ export default ({activeProject, fullWidth}:ScenarioViewProps) => {
   }, []);
 
   const onScenarioCreated = () => {
-      setOpenScenarioDialog(false);
-      fetchScenarios();
-      setAlertState({open: true, message: "Scenario successfully created.", severity: "success"})
+    setOpenScenarioDialog(false);
+    fetchScenarios();
+    setAlertState({ open: true, message: "Scenario successfully created.", severity: "success" })
   };
 
   const onScenarioCreationFailed = () => {
-    setAlertState({open: true, message: "Something failed while creating scenario.", severity: "error"})
+    setAlertState({ open: true, message: "Something failed while creating scenario.", severity: "error" })
   };
 
   const fetchScenarios = () => {
     axios.get(`/api/project/${activeProject}/scenarios`)
-      .then((res:any) => setScenarios(res.data))
+      .then((res: any) => setScenarios(res.data))
       .then(() => setLoadingScenarios(false))
-      .catch((e) => {console.log(e); setLoadingScenarios(false)});
+      .catch((e) => { console.log(e); setLoadingScenarios(false) });
   }
 
   const handleAlertClose = () => {
-    setAlertState({...alertState, open: false})
+    setAlertState({ ...alertState, open: false })
   }
 
-  const handleDelete = (id:string) => {
+  const handleDelete = (id: string) => {
     axios.post(`/api/scenario/${id}/delete`)
       .then(fetchScenarios)
-      .then(() => setWarningState({open: false, id: ""}))
-      .then(() => setAlertState({open: true, message: "Scenario successfully deleted.", severity: "success"}))
-      .catch((e:any) => {
-        setWarningState({open: false, id: ""})
+      .then(() => setWarningState({ open: false, id: "" }))
+      .then(() => setAlertState({ open: true, message: "Scenario successfully deleted.", severity: "success" }))
+      .catch((e: any) => {
+        setWarningState({ open: false, id: "" })
         if (e.response && e.response.status === 409) {
-          setAlertState({open: true, message: "Scenario cannot be deleted because it is still in use.", severity: "warning"})
+          setAlertState({ open: true, message: "Scenario cannot be deleted because it is still in use.", severity: "warning" })
           return
         }
 
-        setAlertState({open: true, message: "Unable to delete scenario.", severity: "error"})
+        setAlertState({ open: true, message: "Unable to delete scenario.", severity: "error" })
       })
   }
 
   const renderOverview = () => {
-    const loading_ = () => range(6).map((elem:number) => {
+    const loading_ = () => range(6).map((elem: number) => {
       return (
         <Grid item xs={12} md={6} lg={3} xl={2} key={elem}>
-          {range(5).map((elem:number) => ( <Skeleton key={elem} animation="wave" /> ))}
+          {range(5).map((elem: number) => (<Skeleton key={elem} animation="wave" />))}
         </Grid>
       )
     })
@@ -232,43 +227,43 @@ export default ({activeProject, fullWidth}:ScenarioViewProps) => {
     const scenarios_ = () => scenarios.length === 0
       ? <Typography variant="subtitle1" component="p">No scenarios have been added yet.</Typography>
       : scenarios.map((scenario: any) => (
-          <ScenarioTile
-            id={scenario.id}
-            key={scenario.id}
-            activeProject={activeProject}
-            name={scenario.name}
-            created_at={scenario.created_at}
-            description={scenario.description}
-            fullWidth={fullWidth}
-            setWarningState={setWarningState}
+        <ScenarioTile
+          id={scenario.id}
+          key={scenario.id}
+          activeProject={activeProject}
+          name={scenario.name}
+          created_at={scenario.created_at}
+          description={scenario.description}
+          fullWidth={fullWidth}
+          setWarningState={setWarningState}
         />
-    ));
+      ));
 
     return (
-      <Grid container spacing={2} className={classes.overview} style={{margin: 0}}>
-        { loadingScenarios ? loading_() : scenarios_() }
+      <Grid container spacing={2} className={classes.overview} style={{ margin: 0 }}>
+        {loadingScenarios ? loading_() : scenarios_()}
       </Grid>
     )
   }
 
   return (
     <Paper elevation={0} variant="outlined" className={classes.paper}>
-      <Typography variant="h4" component="p" className={classes.header}><AccountTreeIcon style={{marginRight:5}}/> 3. Scenarios</Typography>
-      <Grid container spacing={2} style={{margin: 0}}>
-        { renderOverview() }
+      <Typography variant="h4" component="p" className={classes.header}><AccountTreeIcon style={{ marginRight: 5 }} /> 3. Scenarios</Typography>
+      <Grid container spacing={2} style={{ margin: 0 }}>
+        {renderOverview()}
         <Grid item xs={12}>
           <Button
             color="primary"
             onClick={() => setOpenScenarioDialog(true)}
             startIcon={<AddIcon />}
-            style={{marginTop: 10}}
-            >
+            style={{ marginTop: 10 }}
+          >
             Add Scenario
           </Button>
         </Grid>
       </Grid>
-      <DeleteWarningDialog id={warningState.id} open={warningState.open} handleDelete={() => handleDelete(warningState.id)} handleClose={() => setWarningState({open: false, id: ""})} />
-      <NewScenarioDialog activeProject={activeProject} open={openScenarioDialog} closeHandler={setOpenScenarioDialog} onScenarioCreated={onScenarioCreated} onScenarioCreationFailed={onScenarioCreationFailed}/>
+      <DeleteWarningDialog id={warningState.id} open={warningState.open} handleDelete={() => handleDelete(warningState.id)} handleClose={() => setWarningState({ open: false, id: "" })} />
+      <NewScenarioDialog activeProject={activeProject} open={openScenarioDialog} closeHandler={setOpenScenarioDialog} onScenarioCreated={onScenarioCreated} onScenarioCreationFailed={onScenarioCreationFailed} />
       <ScenarioSnackbar open={alertState.open} message={alertState.message} severity={alertState.severity} handleClose={handleAlertClose} />
     </Paper>
   );
--- scene-editor/frontend/src/components/ProjectComponents/ScenarioViewComponents/NewScenarioDialog.tsx
+++ scene-editor/frontend/src/components/ProjectComponents/ScenarioViewComponents/NewScenarioDialog.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -10,81 +10,86 @@ import DialogTitle from '@mui/material/DialogTitle';
 import axios from "axios";
 
 type NewScenarioDialogProps = {
-    activeProject: string;
-    open: boolean;
-    closeHandler: any;
-    onScenarioCreated: any;
-    onScenarioCreationFailed:any;
+  activeProject: string;
+  open: boolean;
+  closeHandler: any;
+  onScenarioCreated: any;
+  onScenarioCreationFailed: any;
 };
 
-const NewScenarioDialog: React.FC<NewScenarioDialogProps> = ({activeProject, open, closeHandler, onScenarioCreated, onScenarioCreationFailed}) => {
-  const token = useSelector((state:any) => state.token)
+const NewScenarioDialog: React.FC<NewScenarioDialogProps> = ({ activeProject, open, closeHandler, onScenarioCreated, onScenarioCreationFailed }) => {
+  const token = useSelector((state: any) => state.token)
   const [name, setName] = useState("")
   const [description, setDescription] = useState("")
 
   const createScenario = async () => {
 
     const payload = {
-        "name": name,
-        "description": description,
+      "name": name,
+      "description": description,
     }
 
     axios
-      .post(`/api/project/${activeProject}/scenarios`, payload )
+      .post(`/api/project/${activeProject}/scenarios`, payload)
       .then((res) => {
-          setName(""); 
-          setDescription(""); 
-          onScenarioCreated(); 
+        setName("");
+        setDescription("");
+        onScenarioCreated();
       })
       .catch((e) => {
         onScenarioCreationFailed();
-    });
-  }; 
+      });
+  };
 
 
   const handleNameChange = (event: any) => {
-    setName (event.target.value);
+    setName(event.target.value);
   };
 
   const handleDescriptionChange = (event: any) => {
-      setDescription(event.target.value);
+    setDescription(event.target.value);
   }
   return (
-      <Dialog open={open} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">New Scenario</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter a name and description for the scenario below.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Scene Name"
-            type="text"
-            fullWidth
-            value={name}
-            onChange={handleNameChange}
-          />
-          <TextField
-            margin="dense"
-            id="name"
-            label="Scene Description"
-            type="text"
-            fullWidth
-            value={description}
-            onChange={handleDescriptionChange}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => {setName(""); setDescription(""); closeHandler(false)} } color="primary">
-            Cancel
-          </Button>
-          <Button onClick={createScenario} color="primary">
-            Create
-          </Button>
-        </DialogActions>
-      </Dialog>
+    <Dialog
+      open={open}
+      aria-labelledby="form-dialog-title"
+      transitionDuration={0}
+      container={() => document.body}
+    >
+      <DialogTitle id="form-dialog-title">New Scenario</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Please enter a name and description for the scenario below.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Scene Name"
+          type="text"
+          fullWidth
+          value={name}
+          onChange={handleNameChange}
+        />
+        <TextField
+          margin="dense"
+          id="name"
+          label="Scene Description"
+          type="text"
+          fullWidth
+          value={description}
+          onChange={handleDescriptionChange}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => { setName(""); setDescription(""); closeHandler(false) }} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={createScenario} color="primary">
+          Create
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 export default NewScenarioDialog;
